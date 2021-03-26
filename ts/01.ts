fdce4e6effe6131ec55a5989bd7c54031ed46bec
1210354825@qq.com <1210354825@qq.com>
2021-03-26 11:54:48 +0800

    add:es6

---
+++ ts/01.ts
@@ -0,0 +1,6 @@
+// 声明的时候，可以限制数据类型（可选），之后会遵循强类型，
+// let num:number = '123';
+let age:number = 20;
+let uname:number ;
+uname = 123;
+console.log( age );
---
+++ ts/note.md
@@ -0,0 +1,6 @@
+# ts 学习笔记
+
+## 环境搭建
+
+### npm 安装 typescript
+`npm install -g typescript`
